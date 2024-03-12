5a520b2e9fbb984cc9edf2cb363a61e305179aca
Samantha Eisenzimmer <samantha.eisenzimmer@mykenzie.snhu.edu>
2024-03-12 11:26:04 -0700

    changing header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import Homepage from './pages/Homepage'
-
+import Homepage from './components/Homepage'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
@@ -10,6 +7,7 @@ function App() {
 
   return (
     <>
+      <Header />
       <Homepage />
     </>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
-import Nav from bootstrap;
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 
 
 function Header() {
     return (
-        <Nav>Header</Nav>
-    )
+        <Navbar expand="lg" className="header">
+            <Container>
+                <Navbar.Brand id="title" href="#home">
+                    {/* <img
+                        alt=""
+                        src="/spork.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '} */}
+                    Trio Digital Dishes</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar id="basic-navbar-nav">
+                    <Nav className="center">
+                        <Form inline="true">
+                            <Row>
+                                <Col xs="auto">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Search"
+                                        className="search"
+                                    />
+                                </Col>
+                                <Col xs="auto">
+                                    <Button id="btn" type="submit">Submit</Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link id="home" href="#home">Home</Nav.Link>
+                        <Nav.Link id="login" href="#login">Log In</Nav.Link>
+                    </Nav>
+
+                    {/* <Nav.Link href="#link">Link</Nav.Link> */}
+
+                </Navbar>
+            </Container>
+        </Navbar>
+    );
 }
 
-export default Header
+export default Header;
