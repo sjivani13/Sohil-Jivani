7b4001d5756bc1329cf52d8f5ad365ff3a4fd88b
johnathanbritt2003 <YOUREMAIL@YOUREMAIL.COM>
2024-03-29 13:23:11 -0400

    fav review button

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -26,11 +26,29 @@ function Dashboard() {
             <h2>Hello, {user.username}</h2>
             <Container style={{ marginTop: "50px" }}>
                 {filteredRecipes.map((recipe) => {
-                    <p>some recipe</p>
+                   <Card style={{ background: "black", width: '18rem', borderRadius: "40%" }}>
+                   <Card.Img src="dinner.jpg" style={{ borderRadius: "30%" }} />
+                   <Card.Text><button> <FavButton recipeId={recipe._id}/> </button>
+                   <button> <ReviewButton/> </button>
+                   </Card.Text>
+                   <Card.Body style={{ background: "black" }}>
+                       <Card.Title style={{ color: "white" }}>Fried Chicken 2.0</Card.Title>
+                       <Card.Text style={{ color: "#b27581" }}>
+                           Some quick example text to build on the card title and make up the
+                           bulk of the card's content.
+                       </Card.Text>
+                       <Button style={{ background: "#b27581" }}
+                           as={Link} to="/post"
+                       >Open Recipe </Button>
+                   </Card.Body>
+
+               </Card>
                 })}
                 <Card style={{ background: "black", width: '18rem', borderRadius: "40%" }}>
                     <Card.Img src="dinner.jpg" style={{ borderRadius: "30%" }} />
-                    <Card.Text><FavButton /> <ReviewButton /> </Card.Text>
+                    <Card.Text><button> <FavButton /> </button>
+                    <button> <ReviewButton/> </button>
+                    </Card.Text>
                     <Card.Body style={{ background: "black" }}>
                         <Card.Title style={{ color: "white" }}>Fried Chicken 2.0</Card.Title>
                         <Card.Text style={{ color: "#b27581" }}>
@@ -53,6 +71,6 @@ function Dashboard() {
     )
 }
 
-export default Dashboard
+export default Dashboard 
 // as={Link} to="/recipe"
 //this is ^^ a link to submit a recipe from the dashboard.
